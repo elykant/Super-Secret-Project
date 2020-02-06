28d2b7269822dff32306e3c74a68092a34e31b33
James S Abreu Mieses <jeandsmith@protonmail.com>
2020-02-06 14:35:29 -0500

    Changes to .gitignore

---
+++ app/src/main/java/com/example/mediamanagament/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mediamanagament;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
